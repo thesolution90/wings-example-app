51a4557daafaf317ef1012ea41ec3a1d241b859b
Christopher Becker <christopher.becker@genesis-group.com>
2024-06-11 11:19:47 +0200

    env variables

--- pipeline/GitHubCommit.py
+++ pipeline/GitHubCommit.py
@@ -14,13 +14,12 @@ class GitHubCommit(GitCommit):
     def __init__(self):
 
         github_commit_message = os.environ.get('GH_COMMIT_MESSAGE')
-        print(github_commit_message)
-        github_commit_hash = os.system('echo "${{ github.event.head_commit.id }}"')
-        github_commit_link_url = os.system('echo "${{ github.event.head_commit.url }}"')
+        github_commit_hash = os.environ.get('GH_COMMIT_HASH')
+        github_commit_link_url = os.environ.get('GH_COMMIT_LINK_URL')
         github_branch_name = os.environ.get('GITHUB_REF_NAME')
-        github_full_name = os.system('echo "${{ github.repository.full_name }}"')
+        github_full_name = os.environ.get('GH_FULL_NAME')
         github_image_name = f'{github_full_name}/{github_commit_hash}'
-        github_project_url = os.system(os.system('echo "${{ github.repository.html_url }}"'))
+        github_project_url = os.environ.get('GH_PROJECT_URL')
         github_pipeline_id = os.environ.get('GITHUB_RUN_ID')
         github_pipeline_link_url = f'{github_project_url}/actions/runs/{github_pipeline_id}'
         github_test_report_link_url = 'placeholder_string'
