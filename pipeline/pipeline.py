661f875bf766f9d19a7cecb763b870aafc2fad8f
Christopher Becker <christopher.becker@genesis-group.com>
2024-06-11 11:53:43 +0200

    env variables

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -28,7 +28,7 @@ if args.step not in allowed_step_strings:
 # Hier werden Instanzen von GitLabCommit und TrelloTask gebildet
 source_system = GitCommit.check_source_system()
 print(source_system)
-git_commit = GitHubCommit() if source_system == 'github' else GitLabCommit()
+git_commit = GitHubCommit() if source_system == 'Github' else GitLabCommit()
 trello = TrelloTask(git_commit)
 
 if args.step == allowed_step_strings[0]:
